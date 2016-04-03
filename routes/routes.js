7d358d94ea3a5b0fd9cd9eec6f9fa3398d0a536f
Ivan Chau <ivan@Wings-MacBook-Air.local>
2016-04-03 14:00:11 -0400

    logout

--- routes/routes.js
+++ routes/routes.js
@@ -8,4 +8,8 @@ app.get('/', function(req,res){
 app.get('/auth/twitter', passport.authenticate('twitter'));
 app.get('/auth/twitter/callback',passport.authenticate('twitter', {successRedirect : '/profileStats', 
 	failureRedirect : '/'}));
+app.get('/logout', function(req,res){
+	req.logout();
+	res.redirect('/');
+});
 module.exports = app;
